90e52650f70abcabbf8e04af80ce260a4af9c97d
Lucas Angelon <lucas.a.arouca@gmail.com>
2016-05-08 17:28:42 +0800

    Add Toast
    Added a Toast to the main page. This is done through java code only and is not linked to the button via XML.

--- app/src/main/java/com/example/lucas/sampleapplication/MainActivity.java
+++ app/src/main/java/com/example/lucas/sampleapplication/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.lucas.sampleapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 // The main menu class for our project.
 public class MainActivity extends AppCompatActivity {
@@ -30,12 +32,32 @@ public class MainActivity extends AppCompatActivity {
     // Setting up some of the buttons in the page.
     private void setupButtons() {
 
+        // Toast Button.
+        Button toastButton = (Button) findViewById(R.id.button_toast);
+        assert toastButton != null;
+        toastButton.setOnClickListener(new View.OnClickListener() {
+
+            // Let's create the toast inside this button.
+            public void onClick(View v) {
+
+                // Defining the required parameters: context, text and time.
+                Context context = getApplicationContext();
+                String text = "You clicked the \"Create Toast\" button!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
 
+    // This button is linked to this action via the XML file, check activity_main.xml for the
+    // onClick event.
     public void basicIntentClick(View v) {
 
         // Setting up a log to check if this button was clicked. Check these out on LogCat.
         Log.i(TAG, "button_basic_intent clicked!");
+
         // Setting up a basic intent and moving onto a new activity, no extra parameters are used.
         Intent intent = new Intent(this, BasicIntentActivity.class);
         startActivity(intent);
